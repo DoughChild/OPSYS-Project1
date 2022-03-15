d850060d66c7ee70d9df77af1bda1bd1798ad4c2
dantk2001 <keegan.dant2143@gmail.com>
2022-03-15 16:16:10 -0400

    Added FCFS and RR .h files

---
+++ FCFS.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <ctype.h>
+#include <iostream>
+#include <queue>
+
+#include "Process.h"
+
+void FCFS(deque<Process*> processes, int t_cs)
+{
+    
+
+}
