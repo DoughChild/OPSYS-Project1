07aed59215400d347958193efbe01e70a57ee12e
Nate Zink <zinkna1@gmail.com>
2022-03-18 01:45:44 -0400

    tau calculation is more accurate
    previous values were usually 1 ms below expected, now match more closely

--- tau_calc.h
+++ tau_calc.h
@@ -5,6 +5,8 @@
 int calc_tau(double prev_tau, int actual, double alpha)
 {
     /* recalculate tau based on previous tau, alpha, and actual time */
-    double new_tau = alpha * actual + (1.0 - alpha) * prev_tau;
-    return (int)ceil(new_tau);
+    // double new_tau = alpha * actual + (1.0 - alpha) * prev_tau;
+    // return (int)ceil(new_tau);
+    double new_tau = ceil(alpha * actual) + ceil((1.0 - alpha) * prev_tau);
+    return (int)new_tau;
 }
