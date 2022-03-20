c957dd408299c2f98a8cc8a131ee63ca98344454
iffat-nafisa <78460366+iffat-nafisa@users.noreply.github.com>
2022-03-19 22:34:25 -0400

    Bug fixes

--- SJF.h
+++ SJF.h
@@ -36,15 +36,16 @@
 using namespace std;
 // Global time counter for process stimulation
 
+vector<int> contextSwitchTimes;
 
 struct shortestJob {
 public:
     bool operator() (Process const *p1, Process const *p2) {
-        if (p1->CPUBursts.at(0) < p2->CPUBursts.at(0)) {
+        if (p1->tau < p2->tau) {
             return true;
         }
 
-        else if (p1->CPUBursts.at(0) > p2->CPUBursts.at(0)) {
+        else if (p1->tau > p2->tau) {
             return false;
         }
         else {
@@ -99,13 +100,13 @@ vector<Process *> processArrived(vector<Process*> processes, int t) {
 }
 
 void printQueue (vector<Process*> processes) {
-    cout << "[Q";
+    cout << "[Q ";
     if (processes.size() == 0) {
-        cout << " empty]" << endl;
+        cout << "empty]" << endl;
         return;
     }
     for (int i = 0; i < processes.size(); i++) {
-        cout << " " << processes[i]->name;
+        cout << processes[i]->name;
     }
     cout << "]" << endl;
 }
@@ -116,12 +117,19 @@ void updateOnCPUBurst(Process *p, int t, int t_cs) {
     p->cur_CPUBurst = p->CPUBursts[0];
     p->CPUBursts.erase(p->CPUBursts.begin());
     p->time_for_next_interesting_event = t + p->cur_CPUBurst;
-    cout << "UPDATE on CPU BURST " << p->time_for_next_interesting_event << endl;
+    // cout << "UPDATE on CPU BURST " << p->time_for_next_interesting_event << endl;
 }
 
 void updateOnSwitchOutOfCPU(Process *p, int t, int t_cs) {
     p->time_for_next_interesting_event = t + (t_cs / 2) + p->IOBursts[0];
     p->IOBursts.erase(p->IOBursts.begin());
+}
+
+void addContextSwitch(int t_cs, int time) {
+    for ( int i = 0; i < t_cs/2; i++) {
+        time += 1;
+        contextSwitchTimes.push_back(time);
+    }
 }
 void SJF(deque<Process*> processes, double tau, int t_cs, double alpha) {
     // error check
@@ -151,26 +159,38 @@ void SJF(deque<Process*> processes, double tau, int t_cs, double alpha) {
     bool CPUActive = false; // cpu not in use
     int releaseCPU = 0;
     int contextSwitchCount = 0;
-
+    int contextSwitchUntil = -99999999;
+    // store each clock time when the CPU is contextswitching
     printf("time %dms: Simulator started for SJF [Q empty]\n", clock);
     while (true) {
 
+//        if (contextSwitchTimes.size() > 0) {
+//            cout << "last contextSwitchTime " << contextSwitchTimes[contextSwitchTimes.size() - 1] << endl;
+//        }
+
         // check for CPU Burst
         if (!CPUActive) {
-            // CPU not in use
-            // check if ready queue is empty or not
-            if (readyQ.size() > 0 && (readyQ.at(0)->time_for_next_interesting_event == clock ||
-                    (readyQ.at(0)->time_for_next_interesting_event < (clock - t_cs / 2) && releaseCPU == clock)) ){
+            // enter if statement
+            bool enter = true;
+
+            if (contextSwitchUntil > 0 && contextSwitchUntil >= clock) {
+                enter = false;
+            }
+            if (enter && readyQ.size() > 0 && ((readyQ.at(0)->time_for_next_interesting_event == clock)  ||
+                    (readyQ.at(0)->time_for_next_interesting_event < (clock - t_cs/2) && releaseCPU == clock)) ){
                 // get the first element
-                cout << "11111111111" << endl;
-                cout << "Time " << clock << endl;
+//                cout << "11111111111" << endl;
+//                cout << "Time " << clock << endl;
+//                cout << "ReleaseCPU " << releaseCPU << " Entered the loop " << clock << "(readyQ.at(0)->time_for_next_interesting_event " << readyQ.at(0)->time_for_next_interesting_event << endl;
+//                cout << "Entered the loop time = " << clock << endl;
                 cpuInUse = readyQ.at(0);
                 CPUActive = true;
                 cpuInUse->in_rq = false;
                 // erase from readyQ
                 readyQ.erase(readyQ.begin());
-                printf("time %dms: Process %c started using the CPU for %dms burst ", clock,
-                       cpuInUse->name, cpuInUse->CPUBursts[0]);
+//                contextSwitchUntil = clock + cpuInUse->CPUBursts[0] + t_cs;
+                printf("time %dms: Process %c (tau %dms) started using the CPU for %dms burst ", clock,
+                       cpuInUse->name, cpuInUse->tau, cpuInUse->CPUBursts[0]);
                 // update releaseCPU
                 printQueue(readyQ);
                 // remove the first CPU burst from the list
@@ -179,7 +199,7 @@ void SJF(deque<Process*> processes, double tau, int t_cs, double alpha) {
 
                 updateOnCPUBurst(cpuInUse, clock, t_cs);
                 releaseCPU = cpuInUse->time_for_next_interesting_event;
-                cout << "Is A using the CPU " << cpuInUse->name << "CPUActive " << CPUActive << endl;
+//                cout << "Is A using the CPU " << cpuInUse->name << "CPUActive " << CPUActive << endl;
             }
         }
 
@@ -190,14 +210,22 @@ void SJF(deque<Process*> processes, double tau, int t_cs, double alpha) {
             // a boolean may be?
             if (cpuInUse->time_for_next_interesting_event == clock) {
                 // if there is more CPU Burst left
-                cout << "CPU is active " << cpuInUse->name << " is using the cpu" << endl;
+//                cout << "CPU is active " << cpuInUse->name << " is using the cpu" << endl;
                 if (cpuInUse->CPUBursts.size() > 0) {
-                    cout << "2222222222222" << endl;
-                    cout << "Time " << clock << endl;
-                    printf("time %dms: Process %c (tau %dms) completed a CPU burst; %lu bursts to go ", clock,
-                           cpuInUse->name, cpuInUse->tau, cpuInUse->CPUBursts.size());
+//                    cout << "2222222222222" << endl;
+//                    cout << "Time " << clock << endl;
+                    if (cpuInUse->CPUBursts.size() == 1) {
+                        printf("time %dms: Process %c (tau %dms) completed a CPU burst; %lu burst to go ", clock,
+                               cpuInUse->name, cpuInUse->tau, cpuInUse->CPUBursts.size());
+                    }
+                    else {
+                        printf("time %dms: Process %c (tau %dms) completed a CPU burst; %lu bursts to go ", clock,
+                               cpuInUse->name, cpuInUse->tau, cpuInUse->CPUBursts.size());
+                    }
+
                     printQueue(readyQ);
-                    int newTau = calc_tau(cpuInUse->tau, clock, alpha);
+                    int newTau = calc_tau(cpuInUse->tau, cpuInUse->cur_CPUBurst, alpha);
+//                    cout << "cpuInUse->cur_CPUBurst " << cpuInUse->cur_CPUBurst << " tau " << cpuInUse->tau << endl;
                     printf("time %dms: Recalculated tau from %dms to %dms for process %c ", clock, cpuInUse->tau
                     , newTau, cpuInUse->name);
                     cpuInUse->tau = newTau;
@@ -213,19 +241,22 @@ void SJF(deque<Process*> processes, double tau, int t_cs, double alpha) {
                     waitingMap[temp->time_for_next_interesting_event].push_back(temp);
                     cpuInUse = NULL;
                     CPUActive = false;
+                    // Context Switch
                 }
                 else { // process terminates
-                    cout << "3333333333333" << endl;
-                    cout << "Time " << clock << endl;
+//                    cout << "3333333333333" << endl;
+//                    cout << "Time " << clock << endl;
                     printf("time %dms: Process %c terminated ", clock, cpuInUse->name);
                     printQueue(readyQ);
                     completedProcess += 1;
-                    cout << "COMPLETED PROCESSES " << completedProcess << endl;
+//                    cout << "COMPLETED PROCESSES " << completedProcess << endl;
                     cpuInUse = NULL;
                     CPUActive = false;
+//                    addContextSwitch(t_cs, clock);
                     // needs to perform context switch
                 }
                 releaseCPU = clock + t_cs;
+                contextSwitchUntil = clock + (t_cs / 2);
             }
         }
 
@@ -237,8 +268,8 @@ void SJF(deque<Process*> processes, double tau, int t_cs, double alpha) {
         if(it != waitingMap.end()) {
             // finish IO for the processes in the list and erase the processes from waitingMap
             for (int i = 0; i < it->second.size(); i++) {
-                cout << "444444444444" << endl;
-                cout << "Time " << clock << endl;
+//                cout << "444444444444" << endl;
+//                cout << "Time " << clock << endl;
                 // finished IO
                 // re-enter the readyQ
                 Process * temp2 = it->second[i];
@@ -252,19 +283,30 @@ void SJF(deque<Process*> processes, double tau, int t_cs, double alpha) {
                 printQueue(readyQ);
                 // if CPU in use
                 // or if readyQ is not empty and the first element is not process
-                if(cpuInUse != NULL || (readyQ.size() != 0 && temp2 != readyQ.at(0))) {
-                    temp2->time_for_next_interesting_event = clock + t_cs;
+                if(CPUActive || (readyQ.size() != 0 && temp2 != readyQ.at(0))) {
+                    temp2->time_for_next_interesting_event = clock;
                 } else {
-                    temp2->time_for_next_interesting_event = clock + t_cs / 2.0;
+                    if (!CPUActive && readyQ.size() > 0 && readyQ.at(0)->name == temp2->name) {
+                        if (contextSwitchUntil > 0 && contextSwitchUntil > clock) {
+                            temp2->time_for_next_interesting_event = contextSwitchUntil + (t_cs / 2.0);
+                        }
+                        else {
+                            temp2->time_for_next_interesting_event = clock + t_cs / 2.0;
+                        }
+                    }
+                    else {
+                        temp2->time_for_next_interesting_event = clock;
+                    }
                 }
                 // increment context switch
                 contextSwitchCount += 1;
             }
+//            // context switch
+//            releaseCPU = clock + (t_cs / 2);
             // erase from map
             waitingMap.erase(it);
         }
 
-        // release CPU
         if (CPUActive && (releaseCPU == clock)) {
             CPUActive = false;
             cout << "6666666666666666" << endl;
@@ -293,13 +335,14 @@ void SJF(deque<Process*> processes, double tau, int t_cs, double alpha) {
                        arrivedProcesses[i]->name, arrivedProcesses[i]->tau);
                 printQueue(readyQ);
                 if (!CPUActive) {
-                    cout << "8888888888888888" << endl;
-                    cout << "Time " << clock << endl;
+//                    cout << "8888888888888888" << endl;
+//                    cout << "Time " << clock << endl;
                     arrivedProcesses[i]->time_for_next_interesting_event = clock + (t_cs / 2);
+//                    addContextSwitch(t_cs, clock);
                 }
                 else {
-                    cout << "99999999999999" << endl;
-                    cout << "Time " << clock << endl;
+//                    cout << "99999999999999" << endl;
+//                    cout << "Time " << clock << endl;
                     arrivedProcesses[i]->time_for_next_interesting_event = clock;
                 }
                 contextSwitchCount += 1;
@@ -307,8 +350,9 @@ void SJF(deque<Process*> processes, double tau, int t_cs, double alpha) {
         }
 
         if (numProcesses == completedProcess) {
-            cout << "101010101010101010101010" << endl;
-            cout << "Time " << clock << endl;
+//            cout << "101010101010101010101010" << endl;
+//            cout << "Time " << clock << endl;
+            addContextSwitch(t_cs, clock);
             clock += (t_cs / 2);
             printf("time %dms: Simulator ended for SJF ", clock);
             printQueue(readyQ);
@@ -316,9 +360,25 @@ void SJF(deque<Process*> processes, double tau, int t_cs, double alpha) {
         }
 //        cout << "CLOCKCLOCKCLOCK" << endl;
 //        cout << "Time " << clock << endl;
+//        cout << "ReleaseCPU " << releaseCPU << endl;
         clock++;
 
     }
 
 }
+
+//        // release CPU
+//        if (CPUActive && (releaseCPU == clock)) {
+//            CPUActive = false;
+//            cout << "6666666666666666" << endl;
+//            cout << "Time " << clock << endl;
+//            if (readyQ.size() > 0) {
+//                readyQ.at(0)->time_for_next_interesting_event = clock + (t_cs / 2);
+//                cout << "77777777777777" << endl;
+//                cout << "Time " << clock << endl;
+//                // context switch starting
+//                releaseCPU = clock + (t_cs / 2);
+//            }
+//        }
+
 #endif //CPUSCHEDULING_SJF_H
