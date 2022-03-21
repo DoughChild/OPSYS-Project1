ff3ba8d96ad175843e37f39a4f9c1a159194fc98
Nate Zink <zinkna1@gmail.com>
2022-03-20 23:36:06 -0400

    tau works
    tau should never be doubted again

--- tau_calc.h
+++ tau_calc.h
@@ -7,6 +7,7 @@ int calc_tau(int prev_tau, int actual, double alpha)
     /* recalculate tau based on previous tau, alpha, and actual time */
     // double new_tau = alpha * actual + (1.0 - alpha) * prev_tau;
     // return (int)ceil(new_tau);
-    double new_tau = ceil(alpha * actual) + ceil((1.0 - alpha) * prev_tau);
+    //double new_tau = ceil(alpha * actual) + ceil((1.0 - alpha) * prev_tau);
+    double new_tau = ceil(alpha * actual + (1.0 - alpha) * prev_tau);
     return (int)new_tau;
 }
